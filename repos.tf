53a7f0314a9c6839e8031ba5c5382e4c8f4784c5
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-02-04 13:16:40 -0800

    Add a cookiecutter-github-control repo

---
+++ repos.tf
@@ -0,0 +1,6 @@
+resource "github_repository" "cookiecutter-github-control" {
+  name        = "cookiecutter-github-control"
+  description = "Template for a GitHub Control repository"
+
+  visibility = "public"
+}
