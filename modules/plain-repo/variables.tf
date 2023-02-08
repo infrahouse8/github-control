5c5f0390eb92c9964b926bfa15ba51486ba4d829
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-02-07 20:21:10 -0800

    Add a repo for infrahouse-toolkit

---
+++ modules/plain-repo/variables.tf
@@ -0,0 +1,11 @@
+variable "repo_description" {
+  description = "The repository description"
+}
+
+variable "repo_name" {
+  description = "Repository short name"
+}
+
+variable "team_id" {
+  description = "Team identifier that has a push permission"
+}
