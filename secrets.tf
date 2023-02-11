e74caa44ce20f18cd8b666f01f079eeb47977b98
infrahouse8 <121983874+infrahouse8@users.noreply.github.com>
2023-02-11 13:59:10 -0800

    Document AWS IAM policy (#29)
    * Document AWS IAM policy
    
    The IAM policy is needed for an AWS user that manages the Terraform
    state and secrets.
    
    * Formatting of course
    
    ---------
    
    Co-authored-by: Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>

--- secrets.tf
+++ secrets.tf
@@ -1,22 +1,29 @@
 resource "aws_secretsmanager_secret" "pypi_api_token" {
   provider                       = aws.uw1
-  name                           = "PYPI_API_TOKEN"
+  name                           = "_github_control__PYPI_API_TOKEN"
   description                    = <<-EOT
 Token for "GitHub Publishing"
 Permissions: Upload packages
 Scope: Entire account (all projects)
+Created in https://pypi.org/manage/account/
 EOT
   force_overwrite_replica_secret = true
   recovery_window_in_days        = 0
 }
 
 
-data "aws_secretsmanager_secret_version" "PYPI_API_TOKEN" {
+data "aws_secretsmanager_secret_version" "pypi_api_token" {
   secret_id = aws_secretsmanager_secret.pypi_api_token.id
 }
 
-resource "github_actions_secret" "infrahouse-toolkit-PYPI_API_TOKEN" {
-  repository      = "infrahouse-toolkit"
-  secret_name     = "PYPI_API_TOKEN"
-  plaintext_value = data.aws_secretsmanager_secret_version.PYPI_API_TOKEN.secret_string
+
+resource "github_actions_secret" "pypi_api_token" {
+  for_each = {
+    for k in keys(local.repos) :
+    k => local.repos[k]
+    if local.repos[k]["type"] == "python_app"
+  }
+  repository      = each.key
+  secret_name     = "PYPI_API_TOKEN" ## Note: not the same name as in aws_secretsmanager_secret.pypi_api_token
+  plaintext_value = data.aws_secretsmanager_secret_version.pypi_api_token.secret_string
 }
