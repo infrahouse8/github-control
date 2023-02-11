139ead935a7695b8bebdada3dbed2991fbb84801
infrahouse8 <121983874+infrahouse8@users.noreply.github.com>
2023-02-10 21:46:10 -0800

    Create PYPI_API_TOKEN in infrahouse-toolkit repo (#28)
    * Create PYPI_API_TOKEN in infrahouse-toolkit repo
    
    The secret value is taken from secretsmanager.
    If the token needs to be rotated, change the value in the secretsmanager
    and run make apply - it will modify the token everywhere where it's
    used.
    
    * formatting
    
    ---------
    
    Co-authored-by: Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>

--- secrets.tf
+++ secrets.tf
@@ -9,3 +9,14 @@ EOT
   force_overwrite_replica_secret = true
   recovery_window_in_days        = 0
 }
+
+
+data "aws_secretsmanager_secret_version" "PYPI_API_TOKEN" {
+  secret_id = aws_secretsmanager_secret.pypi_api_token.id
+}
+
+resource "github_actions_secret" "infrahouse-toolkit-PYPI_API_TOKEN" {
+  repository      = "infrahouse-toolkit"
+  secret_name     = "PYPI_API_TOKEN"
+  plaintext_value = data.aws_secretsmanager_secret_version.PYPI_API_TOKEN.secret_string
+}
