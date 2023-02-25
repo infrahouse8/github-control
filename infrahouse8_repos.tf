76a02d48db9a1ce6faef095be504a50b58967387
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-02-25 13:35:43 -0800

    fix formatting

--- infrahouse8_repos.tf
+++ infrahouse8_repos.tf
@@ -1,10 +1,10 @@
 resource "github_repository" "github-control" {
-  provider = github.infrahouse8
-  name = "github-control"
-  description                 = "InfraHouse GitHub configuration"
-  has_downloads               = false
- has_issues                  = true
- has_projects                = false
- has_wiki                    = false
-vulnerability_alerts        = true
+  provider             = github.infrahouse8
+  name                 = "github-control"
+  description          = "InfraHouse GitHub configuration"
+  has_downloads        = false
+  has_issues           = true
+  has_projects         = false
+  has_wiki             = false
+  vulnerability_alerts = true
 }
