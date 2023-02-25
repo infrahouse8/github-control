9263df2d314894826462b4c9a5f185555c650e03
infrahouse8 <121983874+infrahouse8@users.noreply.github.com>
2023-02-25 14:00:02 -0800

    Import github-control (#38)
    * Import github-control
    
    Importing the repo so it can mange its own properties
    
    * fix formatting
    
    * Add branch protection
    
    * Require checks
    
    * fix repo id
    
    ---------
    
    Co-authored-by: Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>

---
+++ infrahouse8_repos.tf
@@ -0,0 +1,29 @@
+resource "github_repository" "github-control" {
+  provider = github.infrahouse8
+
+  name                 = "github-control"
+  description          = "InfraHouse GitHub configuration"
+  has_downloads        = false
+  has_issues           = true
+  has_projects         = false
+  has_wiki             = false
+  vulnerability_alerts = true
+}
+
+resource "github_branch_default" "github-control-main" {
+  provider = github.infrahouse8
+
+  branch     = "main"
+  repository = github_repository.github-control.name
+}
+
+resource "github_branch_protection" "github-control-main" {
+  provider = github.infrahouse8
+
+  pattern       = github_branch_default.github-control-main.branch
+  repository_id = github_repository.github-control.id
+
+  required_status_checks {
+    strict = true
+  }
+}
