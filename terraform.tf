083d434679991af2112bd898416fd0b2b5d830be
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-02-25 22:42:40 -0800

    Use github-admin role
    In different environments different users/entities may run terraform
    plan/apply.
    
    For example, now my local account runs it as well as a user in GitHub
    actions.
    
    To ensure consistent permissions, whatever entity runs the terraform
    commands it will use the same IAM role `github-admin`.
    
    (no changes in resources are expected)

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "infrahouse-github-state"
-    key    = "github.state"
+    bucket   = "infrahouse-github-state"
+    key      = "github.state"
+    region   = "us-west-1"
+    role_arn = "arn:aws:iam::990466748045:role/github-admin"
   }
+
   required_providers {
     github = {
       source  = "integrations/github"
