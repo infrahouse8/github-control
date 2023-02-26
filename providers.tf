bb35be058429a6ec7ba00a14c80531da2aba9e68
infrahouse8 <121983874+infrahouse8@users.noreply.github.com>
2023-02-26 10:41:43 -0800

    Use github-admin role (#42)
    * Use github-admin role
    
    In different environments different users/entities may run terraform
    plan/apply.
    
    For example, now my local account runs it as well as a user in GitHub
    actions.
    
    To ensure consistent permissions, whatever entity runs the terraform
    commands it will use the same IAM role `github-admin`.
    
    (no changes in resources are expected)
    
    * Use infrahouse-toolkit 1.2 that supports AWS roles
    
    * oops, use role to upload, not publish.
    
    ---------
    
    Co-authored-by: Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>

--- providers.tf
+++ providers.tf
@@ -9,10 +9,23 @@ provider "github" {
 
 provider "aws" {
   region = "us-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::990466748045:role/github-admin"
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
   alias  = "uw1"
+  assume_role {
+    role_arn = "arn:aws:iam::990466748045:role/github-admin"
+  }
 }
 
 provider "aws" {
   region = "us-west-2"
   alias  = "uw2"
+  assume_role {
+    role_arn = "arn:aws:iam::990466748045:role/github-admin"
+  }
 }
