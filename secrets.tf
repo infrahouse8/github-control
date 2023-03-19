23afe7106000f4ea7aba822661efb42f8e4e9b90
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-03-19 14:13:54 -0700

    Share GHA secrets with created TF repos

--- secrets.tf
+++ secrets.tf
@@ -27,3 +27,33 @@ resource "github_actions_secret" "pypi_api_token" {
   secret_name     = "PYPI_API_TOKEN" ## Note: not the same name as in aws_secretsmanager_secret.pypi_api_token
   plaintext_value = data.aws_secretsmanager_secret_version.pypi_api_token.secret_string
 }
+
+
+data "aws_secretsmanager_secret" "aws_access_key_id" {
+  for_each = {
+    for k in keys(local.repos) :
+    k => local.repos[k]
+    if local.repos[k]["type"] == "terraform_aws"
+  }
+  name = "tf_admin/${each.value["tf_admin_username"]}"
+}
+
+data "aws_secretsmanager_secret_version" "aws_access_key_id" {
+  for_each = {
+    for k in keys(local.repos) :
+    k => local.repos[k]
+    if local.repos[k]["type"] == "terraform_aws"
+  }
+  secret_id = data.aws_secretsmanager_secret.aws_access_key_id["tf_admin/${each.value["tf_admin_username"]}"].id
+}
+
+resource "github_actions_secret" "aws_access_key_id" {
+  for_each = {
+    for k in keys(local.repos) :
+    k => local.repos[k]
+    if local.repos[k]["type"] == "terraform_aws"
+  }
+  repository      = each.key
+  secret_name     = "aws_access_key_id"
+  plaintext_value = data.aws_secretsmanager_secret_version.aws_access_key_id["tf_admin/${each.value["tf_admin_username"]}"].secret_string
+}
