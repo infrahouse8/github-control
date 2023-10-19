6b1bc9ca4bb91619d025e369905f06ea664dd246
infrahouse8 <121983874+infrahouse8@users.noreply.github.com>
2023-10-19 09:46:59 -0700

    Add crated_by tag to all resources created by aws provider (#104)
    Co-authored-by: Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "developers" {
   default     = []
   type        = list(string)
 }
+
+variable "default_tags" {
+  description = "A map with tags that will be applied to all resources created by the AWS provider"
+  type        = map(string)
+}
