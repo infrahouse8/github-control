12de33d777c9197f247a1b72175699d514e7f18a
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-11-08 04:54:11 -0800

    Sort repos by name
    To make it easier to manage the list of repos, sort them by the name.

--- repos.tf
+++ repos.tf
@@ -1,143 +1,124 @@
 locals {
   repos = {
-    "aws-control" : {
-      description = "InfraHouse Main AWS Account 990466748045."
-      team_id     = github_team.dev.id
-      type        = "terraform_aws"
+    "aws-control" = {
+      "description" = "InfraHouse Main AWS Account 990466748045."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_aws"
     }
-
-    "aws-control-289256138624" : {
-      description = "InfraHouse Terraform Control AWS Account 289256138624."
-      team_id     = github_team.dev.id
-      type        = "terraform_aws"
+    "aws-control-289256138624" = {
+      "description" = "InfraHouse Terraform Control AWS Account 289256138624."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_aws"
     }
-
-    "aws-control-303467602807" : {
-      description = "InfraHouse CI/CD AWS Account 303467602807."
-      team_id     = github_team.dev.id
-      type        = "terraform_aws"
+    "aws-control-303467602807" = {
+      "description" = "InfraHouse CI/CD AWS Account 303467602807."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_aws"
     }
-
-    "aws-control-493370826424" : {
-      description = "InfraHouse Management AWS Account 493370826424."
-      team_id     = github_team.dev.id
-      type        = "terraform_aws"
+    "aws-control-493370826424" = {
+      "description" = "InfraHouse Management AWS Account 493370826424."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_aws"
     }
-
-    "infrahouse-toolkit" : {
-      description = "InfraHouse Toolkit."
-      team_id     = github_team.dev.id
-      type        = "python_app"
-      secrets = {
-        PYPI_API_TOKEN       = data.aws_secretsmanager_secret_version.pypi_api_token.secret_string
-        CODACY_PROJECT_TOKEN = data.aws_secretsmanager_secret_version.codacy_api_token.secret_string
+    "cookiecutter-github-control" = {
+      "description" = "Template for a GitHub Control repository."
+      "team_id"     = github_team.dev.id
+      "type"        = "other"
+    }
+    "infrahouse-com" = {
+      "description" = "InfraHouse.com content."
+      "secrets" = {
+        "AWS_DEFAULT_REGION" = local.aws_default_region
       }
+      "team_id" = github_team.dev.id
+      "type"    = "other"
     }
-
-    "infrahouse-website-infra" : {
-      description = "InfraHouse Website Infrastructure."
-      team_id     = github_team.dev.id
-      type        = "terraform_aws"
-    }
-
-    "infrahouse-com" : {
-      description = "InfraHouse.com content."
-      team_id     = github_team.dev.id
-      type        = "other"
-      secrets = {
-        AWS_DEFAULT_REGION = local.aws_default_region
+    "infrahouse-toolkit" = {
+      "description" = "InfraHouse Toolkit."
+      "secrets" = {
+        "CODACY_PROJECT_TOKEN" = data.aws_secretsmanager_secret_version.codacy_api_token.secret_string
+        "PYPI_API_TOKEN"       = data.aws_secretsmanager_secret_version.pypi_api_token.secret_string
       }
+      "team_id" = github_team.dev.id
+      "type"    = "python_app"
     }
-
-    "cookiecutter-github-control" : {
-      description = "Template for a GitHub Control repository."
-      team_id     = github_team.dev.id
-      type        = "other"
+    "infrahouse-website-infra" = {
+      "description" = "InfraHouse Website Infrastructure."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_aws"
     }
-
-    "proxysql-sandbox" : {
-      description = "Terraform live module to deploy ProxySQL sandbox on AWS."
-      team_id     = github_team.dev.id
-      type        = "other"
+    "proxysql-sandbox" = {
+      "description" = "Terraform live module to deploy ProxySQL sandbox on AWS."
+      "team_id"     = github_team.dev.id
+      "type"        = "other"
     }
-
-    "puppet-code" : {
-      description = "Puppet Configuration. Modules, Manifests and Hiera."
-      team_id     = github_team.dev.id
-      type        = "other"
-      secrets = {
-        AWS_DEFAULT_REGION = local.aws_default_region
+    "puppet-code" = {
+      "description" = "Puppet Configuration. Modules, Manifests and Hiera."
+      "secrets" = {
+        "AWS_DEFAULT_REGION" = local.aws_default_region
       }
+      "team_id" = github_team.dev.id
+      "type"    = "other"
     }
-
-    "terraform-aws-service-network" : {
-      description = "Terraform service network module."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-ci-cd" = {
+      "description" = "Module that creates roles, state bucket, and dynamodb table for Terraform CI/CD."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-gha-admin" : {
-      description = "Module for two roles to manage AWS with GitHub actions."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-cloud-init" = {
+      "description" = "Module that creates an cloud init configuration for an InfraHouse EC2 instance."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-state-manager" : {
-      description = "Module creates an IAM role that can manage a Terraform state."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-debian-repo" = {
+      "description" = "Module that creates a Debian repository backed by S3 and fronted by CloudFront."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-gh-identity-provider" : {
-      description = "Module that configures GitHub OpenID connector."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-gh-identity-provider" = {
+      "description" = "Module that configures GitHub OpenID connector."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-instance-profile" : {
-      description = "Module bundles AWS resources to create an instance profile."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-gha-admin" = {
+      "description" = "Module for two roles to manage AWS with GitHub actions."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-debian-repo" : {
-      description = "Module that creates a Debian repository backed by S3 and fronted by CloudFront."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-github-role" = {
+      "description" = "Module that creates a role for a GitHub Action worker."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-website-pod" : {
-      description = "Module that creates an autoscaling group with an ALB and SSL certificate for a website."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-instance-profile" = {
+      "description" = "Module bundles AWS resources to create an instance profile."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-cloud-init" : {
-      description = "Module that creates an cloud init configuration for an InfraHouse EC2 instance."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-jumphost" = {
+      "description" = "Module that creates a jumphost."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-state-bucket" : {
-      description = "Module that creates an S3 bucket for a Terraform state."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-service-network" = {
+      "description" = "Terraform service network module."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-
-    "terraform-aws-ci-cd" : {
-      description = "Module that creates roles, state bucket, and dynamodb table for Terraform CI/CD."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-state-bucket" = {
+      "description" = "Module that creates an S3 bucket for a Terraform state."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-    "terraform-aws-jumphost" : {
-      description = "Module that creates a jumphost."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-state-manager" = {
+      "description" = "Module creates an IAM role that can manage a Terraform state."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
-    "terraform-aws-github-role" : {
-      description = "Module that creates a role for a GitHub Action worker."
-      team_id     = github_team.dev.id
-      type        = "terraform_module"
+    "terraform-aws-website-pod" = {
+      "description" = "Module that creates an autoscaling group with an ALB and SSL certificate for a website."
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
     }
   }
 }
