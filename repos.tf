36a858c9e54bb075b6e10eb23df968c01e9ff467
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-02-17 18:09:40 -0800

    Add repo terraform-aws-kibana

--- repos.tf
+++ repos.tf
@@ -93,6 +93,11 @@ locals {
       "team_id"     = github_team.dev.id
       "type"        = "terraform_module"
     }
+    "terraform-aws-kibana" = {
+      "description" = "Module that deploys Kibana"
+      "team_id"     = github_team.dev.id
+      "type"        = "terraform_module"
+    }
     "terraform-aws-gh-identity-provider" = {
       "description" = "Module that configures GitHub OpenID connector."
       "team_id"     = github_team.dev.id
