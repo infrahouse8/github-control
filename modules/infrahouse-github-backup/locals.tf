9db50a2ae60843ed7790ae51a328a76808c40ace
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-10-08 12:15:22 -0700

    Add jumphost role to infrahouse-github-backup (#133)

--- modules/infrahouse-github-backup/locals.tf
+++ modules/infrahouse-github-backup/locals.tf
@@ -1,3 +1,6 @@
 locals {
-  infrahouse_role = "arn:aws:iam::303467602807:role/aws-reserved/sso.amazonaws.com/us-west-1/AWSReservedSSO_AWSAdministratorAccess_422821c726d81c14"
+  infrahouse_roles = [
+    "arn:aws:iam::303467602807:role/aws-reserved/sso.amazonaws.com/us-west-1/AWSReservedSSO_AWSAdministratorAccess_422821c726d81c14",
+    "arn:aws:iam::493370826424:instance-profile/jumphost-Zv01ElhajbB6",
+  ]
 }
