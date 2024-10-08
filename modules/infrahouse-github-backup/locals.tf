60684b99c7b494306209f1b1fc00a862d1eb2757
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-10-08 12:19:03 -0700

    Fix typo. Should be role, not instance profile

--- modules/infrahouse-github-backup/locals.tf
+++ modules/infrahouse-github-backup/locals.tf
@@ -1,6 +1,6 @@
 locals {
   infrahouse_roles = [
     "arn:aws:iam::303467602807:role/aws-reserved/sso.amazonaws.com/us-west-1/AWSReservedSSO_AWSAdministratorAccess_422821c726d81c14",
-    "arn:aws:iam::493370826424:instance-profile/jumphost-Zv01ElhajbB6",
+    "arn:aws:iam::493370826424:role/jumphost-Zv01ElhajbB620240223005112032800000005",
   ]
 }
