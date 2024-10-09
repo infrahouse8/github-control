ca4b9a3a7eb657dc784e2ebe3e6ec78dbda709b2
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-10-09 09:56:11 -0700

    Use published github-backup-configuration

--- infrahouse-github-backup.tf
+++ infrahouse-github-backup.tf
@@ -2,5 +2,6 @@ module "infrahouse-github-backup" {
   providers = {
     aws = aws.aws-303467602807-uw1
   }
-  source = "./modules/infrahouse-github-backup"
+  source  = "infrahouse/github-backup-configuration/aws"
+  version = "0.1.0"
 }
--- modules/infrahouse-github-backup/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  infrahouse_roles = [
-    "arn:aws:iam::493370826424:role/infrahouse-github-backup",
-  ]
-}
