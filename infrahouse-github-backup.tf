92097fe251de28052c69285945d6491f444e83fa
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-10-09 09:56:33 -0700

    Use published github-backup-configuration

--- infrahouse-github-backup.tf
+++ infrahouse-github-backup.tf
@@ -2,6 +2,6 @@ module "infrahouse-github-backup" {
   providers = {
     aws = aws.aws-303467602807-uw1
   }
-  source  = "infrahouse/github-backup-configuration/aws"
+  source  = "registry.infrahouse.com/infrahouse/github-backup-configuration/aws"
   version = "0.1.0"
 }
