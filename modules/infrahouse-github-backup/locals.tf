aa5b182cade87e88a4a3b3b1d63a900af7fdb591
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-10-08 20:14:18 -0700

    Set role to infrahouse-github-backup (#136)
    The role `arn:aws:iam::493370826424:role/infrahouse-github-backup` is
    expected to be final. It's a role the infrahouse-github-backup EC2
    instance gets via a profile.

--- modules/infrahouse-github-backup/locals.tf
+++ modules/infrahouse-github-backup/locals.tf
@@ -1,6 +1,5 @@
 locals {
   infrahouse_roles = [
-    "arn:aws:iam::303467602807:role/aws-reserved/sso.amazonaws.com/us-west-1/AWSReservedSSO_AWSAdministratorAccess_422821c726d81c14",
-    "arn:aws:iam::493370826424:role/jumphost-Zv01ElhajbB620240223005112032800000005",
+    "arn:aws:iam::493370826424:role/infrahouse-github-backup",
   ]
 }
