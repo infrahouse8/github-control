7cd7e8e51a37a0f87d4bfd74c38167aa8c4cb1bc
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-10-09 17:50:11 -0700

    Add repo secret with infrahouse-github-terraform github app key (#140)

--- infrahouse8_repos.tf
+++ infrahouse8_repos.tf
@@ -19,7 +19,8 @@ module "ih_8_repos" {
   template_repo    = contains(keys(each.value), "template_repo") ? each.value["template_repo"] : null
   secrets = merge(contains(keys(each.value), "secrets") ? each.value["secrets"] : {},
     {
-      GH_TOKEN = data.external.env.result["GH_TOKEN"]
+      GH_TOKEN         = data.external.env.result["GH_TOKEN"]
+      IH_GH_TF_APP_KEY = module.infrahouse-github-terraform-pem.secret_value
     }
   )
   collaborators = [
