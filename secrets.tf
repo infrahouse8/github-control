e76f0ecf17a887c50d8c6cda349300ab45442cb1
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-11-16 10:58:57 -0800

    Chnaging policy for an experiment

--- secrets.tf
+++ secrets.tf
@@ -30,9 +30,12 @@ module "actions-runner-pem" {
   secret_description = "A copy of infrahouse-github-terraform App private key (pem) for actions-runner tests"
   secret_name_prefix = "action-runner-pem-"
   secret_value       = module.infrahouse-github-terraform-pem.secret_value
+  admins = [
+    tolist(data.aws_iam_roles.sso-admin.arns)[0],
+  ]
   readers = [
     data.aws_iam_role.actions-runner-tester.arn,
-    tolist(data.aws_iam_roles.sso-admin.arns)[0],
-    "arn:aws:iam::303467602807:role/infrahouse-registration*"
+    # tolist(data.aws_iam_roles.sso-admin.arns)[0],
+    # "arn:aws:iam::303467602807:role/infrahouse-registration*"
   ]
 }
